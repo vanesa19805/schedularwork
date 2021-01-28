ade3f989dc7748bb2f9d6ba53c07af6cc963c807
vanesa gonzalez <gvane1919@gmail.com>
2021-01-27 22:42:50 -0500

    updated with comments

--- javascript.js
+++ javascript.js
@@ -1,9 +1,11 @@
  
-//var date= {month: 'long', day:'2-digit}
+//THEN the current day is displayed at the top of the calendar *
 
 $("#currentDay").text(moment().format("dddd, MMMM Do"))
 
-
+//if event has passed time block, change color to gray -using past time css
+//if we are current  in in this time change color to red - using present css
+// if event has not passed keep block green- using future css
 var currentHour= moment().hour()
 
 var myTime= [9,10,11,12,13,14,15,16,17]
@@ -26,12 +28,17 @@ for (let i = 0; i < myTime.length; i++) {
     }
 }
 
+
+//WHEN I click into a time block
+//THEN I can enter an event
+//WHEN I click the save button for that time block
+//THEN the text for that event is saved in local storage
+//WHEN I refresh the page
+//THEN the saved events persist
+
 $(".input-group-text").on("click",function(){
     var dataID= $(this).attr("data-id")
     var event= $("#"+dataID).val()
     localStorage.setItem(dataID,event)
 })
 
-//if event has passed time block, change color to gray -using past time css
-//if we are current  in in this time change color to red - using present css
-// if event has not passed keep block green- using future css
